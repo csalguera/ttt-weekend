0f9e8f110ddf89c873accf350a380ad42764aade
Carlos Salguera <carlos.e.salguera@gmail.com>
2022-12-08 16:12:11 -0500

    add variables, const els, functions init and render

--- js/app.js
+++ js/app.js
@@ -4,11 +4,15 @@
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-
+let board
+let turn
+let winner
+let tie
 
 /*------------------------ Cached Element References ------------------------*/
 
-
+const squareEls = document.querySelector('.board')
+const messageEl = document.getElementById('board')
 
 /*----------------------------- Event Listeners -----------------------------*/
 
@@ -16,3 +20,30 @@
 
 /*-------------------------------- Functions --------------------------------*/
 
+window.onload = function init() {
+  board = [null, null, null, null, null, null, null, null, null]
+  turn = 1
+  winner = false
+  tie = false
+  render()
+  updateBoard()
+}
+
+function render() {
+  
+}
+
+// // 1) Define the required variables used to track the state of the game
+
+// // 2) Store cached element references
+
+// // 3) Upon loading, the game state should be initialized, and a function should be 
+// //   called to render this game state
+
+// 4) The state of the game should be rendered to the user
+
+// 5) Define the required constants
+
+// 6) Handle a player clicking a square with a `handleClick` function
+
+// 7) Create Reset functionality
