366886cd6f64cfa5dff5c8344198132a0fcc5b75
Carlos Salguera <carlos.e.salguera@gmail.com>
2022-12-09 17:11:02 -0500

    add reset button and fix updateBoard function

--- js/app.js
+++ js/app.js
@@ -22,14 +22,19 @@ let tie
 
 const squareEls = document.querySelectorAll('.sqr')
 const messageEl = document.getElementById('board')
+const resetBtnEl = document.querySelector('button')
+
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 squareEls.forEach(squareEl => squareEl.addEventListener('click', handleClick))
+resetBtnEl.addEventListener('click', init)
 
 /*-------------------------------- Functions --------------------------------*/
 
-window.onload = function init() {
+window.onload = init
+
+function init() {
   board = [null, null, null, null, null, null, null, null, null]
   turn = 1
   winner = false
@@ -45,7 +50,7 @@ function render() {
 function updateBoard() {
   board.forEach(function(square, idx) {
     if (square === null) {
-      return
+      squareEls[idx].textContent = null
     } else if (square === 1) {
       squareEls[idx].textContent = 'x'
     } else if (square === -1) {
@@ -113,18 +118,5 @@ function switchPlayerTurn() {
     turn *= -1
   }
 }
-
-// // 1) Define the required variables used to track the state of the game
-
-// // 2) Store cached element references
-
-// // 3) Upon loading, the game state should be initialized, and a function should be 
-// //   called to render this game state
-
-// // 4) The state of the game should be rendered to the user
-
-// // 5) Define the required constants
-
-// 6) Handle a player clicking a square with a `handleClick` function
 
 // 7) Create Reset functionality
